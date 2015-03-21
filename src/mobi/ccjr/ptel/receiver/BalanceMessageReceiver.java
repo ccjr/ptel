6cd4dc9d63ac08a3fe629dbbb3e21742b7a0f889
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2015-03-21 15:15:52 -0400

    Parsing balance out of incoming sms

--- src/mobi/ccjr/ptel/receiver/BalanceMessageReceiver.java
+++ src/mobi/ccjr/ptel/receiver/BalanceMessageReceiver.java
@@ -1,10 +1,12 @@
 package mobi.ccjr.ptel.receiver;
 
+import mobi.ccjr.ptel.parser.BalanceMessageParser;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 public class BalanceMessageReceiver extends BroadcastReceiver {
 
@@ -12,15 +14,21 @@ public class BalanceMessageReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		Bundle bundle = intent.getExtras();
 		SmsMessage[] msgs = null;
-		String messageReceived = "";
 		if (bundle != null) {
 			// ---retrieve the SMS message received---
 			Object[] pdus = (Object[]) bundle.get("pdus");
 			msgs = new SmsMessage[pdus.length];
 			for (int i = 0; i < msgs.length; i++) {
 				msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-				// TODO: parse and extract balance
-				messageReceived += msgs[i].getMessageBody().toString();
+				String messageReceived = msgs[i].getMessageBody().toString();
+
+				BalanceMessageParser parser = new BalanceMessageParser(
+						messageReceived);
+				// TODO update balance somewhere
+				Toast toast = Toast.makeText(context,
+						"message: " + parser.extractBalance(),
+						Toast.LENGTH_LONG);
+				toast.show();
 			}
 		}
 	}
