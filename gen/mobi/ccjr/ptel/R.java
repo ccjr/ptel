fc1f52a1cf50fd548ac1f8394a2444c37af85ffb
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2015-03-21 00:01:57 -0400

    Created send sms fragment

--- gen/mobi/ccjr/ptel/R.java
+++ gen/mobi/ccjr/ptel/R.java
@@ -25,18 +25,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int parent_hierarchy=0x7f080000;
+        public static final int send_sms_button=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_send=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int check_balance=0x7f050001;
+        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
---
+++ src/mobi/ccjr/ptel/fragments/SendFragment.java
@@ -0,0 +1,36 @@
+package mobi.ccjr.ptel.fragments;
+
+import mobi.ccjr.ptel.R;
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SendFragment extends Fragment implements OnClickListener {
+
+	private Button sendButton;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View v = inflater.inflate(R.layout.fragment_send, container, false);
+		sendButton = (Button) v.findViewById(R.id.send_sms_button);
+		sendButton.setOnClickListener(this);
+		return v;
+	}
+
+	@SuppressLint("UnlocalizedSms")
+	@Override
+	public void onClick(View v) {
+		// send SMS with the word BAL to 7801
+		SmsManager smsManager = SmsManager.getDefault();
+		smsManager.sendTextMessage("7801", null, "BAL", null, null);
+	}
+
+}
