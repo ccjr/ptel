23b66443984eed24a2b8ddffcd6acf208f6ed9e4
Cloves Carneiro <ccarneiroj@gmail.com>
2015-03-25 00:44:59 -0400

    Saving balance to a preference

--- app/src/main/java/mobi/ccjr/ptel/receiver/BalanceMessageReceiver.java
+++ app/src/main/java/mobi/ccjr/ptel/receiver/BalanceMessageReceiver.java
@@ -1,10 +1,13 @@
 package mobi.ccjr.ptel.receiver;
 
+import mobi.ccjr.ptel.R;
 import mobi.ccjr.ptel.parser.BalanceMessageParser;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
@@ -20,16 +23,23 @@ public class BalanceMessageReceiver extends BroadcastReceiver {
 			msgs = new SmsMessage[pdus.length];
 			for (int i = 0; i < msgs.length; i++) {
 				msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-				String messageReceived = msgs[i].getMessageBody().toString();
-
-				BalanceMessageParser parser = new BalanceMessageParser(
-						messageReceived);
-				// TODO update balance somewhere
-				Toast toast = Toast.makeText(context,
-						"message: " + parser.extractBalance(),
-						Toast.LENGTH_LONG);
-				toast.show();
+				String message = msgs[i].getMessageBody().toString();
+                persistBalance(context, message);
 			}
 		}
 	}
+
+    private void persistBalance(Context context, String message) {
+        BalanceMessageParser parser = new BalanceMessageParser(message);
+
+        // TODO move this code to its own class
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(context.getString(R.string.saved_balance), parser.extractBalance());
+        editor.commit();
+
+        Toast toast = Toast.makeText(context, "message: " + parser.extractBalance(),
+            Toast.LENGTH_LONG);
+        toast.show();
+    }
 }
