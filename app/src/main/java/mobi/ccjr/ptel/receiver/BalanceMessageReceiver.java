1ab3817e5e3b8477bd845e30379bf26ba3e09fa1
Cloves Carneiro <ccarneiroj@gmail.com>
2015-03-25 10:59:39 -0400

    Code formatting

--- app/src/main/java/mobi/ccjr/ptel/receiver/BalanceMessageReceiver.java
+++ app/src/main/java/mobi/ccjr/ptel/receiver/BalanceMessageReceiver.java
@@ -1,8 +1,5 @@
 package mobi.ccjr.ptel.receiver;
 
-import mobi.ccjr.ptel.R;
-import mobi.ccjr.ptel.parser.BalanceMessageParser;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,7 +9,11 @@ import android.preference.PreferenceManager;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
-public class BalanceMessageReceiver extends BroadcastReceiver {
+import mobi.ccjr.ptel.R;
+import mobi.ccjr.ptel.parser.BalanceMessageParser;
+
+public class BalanceMessageReceiver
+        extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -24,7 +25,8 @@ public class BalanceMessageReceiver extends BroadcastReceiver {
             msgs = new SmsMessage[pdus.length];
             for (int i = 0; i < msgs.length; i++) {
                 msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                String message = msgs[i].getMessageBody().toString();
+                String message = msgs[i].getMessageBody()
+                                        .toString();
                 persistBalance(context, message);
             }
         }
@@ -39,8 +41,9 @@ public class BalanceMessageReceiver extends BroadcastReceiver {
         editor.putString(context.getString(R.string.saved_balance), parser.extractBalance());
         editor.commit();
 
-        Toast toast = Toast.makeText(context, "message: " + parser.extractBalance(),
-                Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(context,
+                                     "message: " + parser.extractBalance(),
+                                     Toast.LENGTH_LONG);
         toast.show();
     }
 }
