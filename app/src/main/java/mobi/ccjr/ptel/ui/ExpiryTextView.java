8ec1d827688befe5b74e78e155c5f471d5f98af6
Cloves Carneiro <ccarneiroj@gmail.com>
2015-03-28 10:37:30 -0400

    Setting expiry text view in alarm state based on user preferences

--- app/src/main/java/mobi/ccjr/ptel/ui/ExpiryTextView.java
+++ app/src/main/java/mobi/ccjr/ptel/ui/ExpiryTextView.java
@@ -12,7 +12,7 @@ import java.util.Date;
 
 import mobi.ccjr.ptel.R;
 import mobi.ccjr.ptel.model.Balance;
-import mobi.ccjr.ptel.utils.Constants;
+import mobi.ccjr.ptel.model.UserPreference;
 import mobi.ccjr.ptel.utils.DateCalculation;
 
 public class ExpiryTextView
@@ -34,23 +34,22 @@ public class ExpiryTextView
     public void setBalance(Balance balance) {
         this.balance = balance;
         this.setText(getExpiryText());
-        if (daysUntilExpiry() > Constants.EXPIRY_ALARM_DEFAULT_THRESHOLD_IN_DAYS) {
-            this.setTextColor(getResources().getColor(R.color.normal_blue));
-        } else {
+        if (inAlarmState()) {
             this.setTextColor(getResources().getColor(R.color.red_alarm));
+            this.blink();
         }
-        this.blink();
+        else {
+            this.setTextColor(getResources().getColor(R.color.normal_blue));
+        }
+    }
+
+    private boolean inAlarmState() {
+        return balance.daysUntilExpiry() <= UserPreference.expiryThresholdInDays(getContext());
     }
 
     private Spanned getExpiryText() {
         String html = "<small>credit expires in</small><br>" + getExpiryInWords();
         return Html.fromHtml(html);
-    }
-
-    private long daysUntilExpiry() {
-        Date date1 = new Date();
-        Date date2 = balance.getExpiryAsDate();
-        return DateCalculation.daysBetweenDates(date1, date2);
     }
 
     private void blink() {
@@ -63,7 +62,7 @@ public class ExpiryTextView
 
     private String getExpiryInWords() {
         // TODO: support expiring today and in past
-        long days = daysUntilExpiry();
+        long days = balance.daysUntilExpiry();
         return days + " days";
     }
 }
